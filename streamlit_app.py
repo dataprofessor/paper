5d59c3f47928201de42a8ca45aa6fa7ba7447859
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-10-06 17:15:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ st.info('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 ''')
 
-st.markdown('**Keywords:** `Streamlit`, `machine learning`, `data science`, `Python`)
+st.markdown('**Keywords:** `Streamlit`, `machine learning`, `data science`, `Python`')
 
 st.header('Introduction')
 st.markdown('''
