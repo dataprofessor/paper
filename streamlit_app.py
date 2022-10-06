13891bf7dc48c90789a7a6292e4156d9aebc9656
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-10-06 16:30:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,24 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Bioinformatics App Starter Kit: Towards rapid creation of web applications in bioinformatics')
 
-st.write('Hello world!')
+st.markdown('**Abstract**')
+st.info('''
+
+''')
+
+st.markdown('''
+
+With the ever-growing importance of data, bioinformatics has become increasingly important. Bioinformatics is the field of science that deals with the acquisition, management, and analysis of biological data. In order to make sense of the vast amount of data that is generated every day, bioinformaticians rely on computers and software to help them Organize, store, and analyze this data.
+
+One of the most important tools that bioinformaticians use is web applications. Web applications are software programs that run on a web server and are accessed by users through a web browser. They are typically used to provide interactive features that allow users to view, edit, and analyze data.
+
+There are many reasons why web applications are so important for bioinformatics. First, they allow bioinformaticians to access data from anywhere in the world. Second, they provide a user-friendly interface that makes it easy to view and analyze data. Third, they allow bioinformaticians to share data and collaborate with other scientists.
+
+Fourth, web applications can be used to create custom tools that are specific to the needs of a particular project. For example, a bioinformatician working on a genomics project may need a tool that allows them to view and compare the genomes of different organisms. By using a web application, they can create this tool and make it available to other scientists working on the same project.
+
+Finally, web applications can be used to create public databases that are accessible to anyone with an internet connection. These databases can be used to store and share data, and they can be used by bioinformaticians to find new and interesting patterns in the data.
+
+The importance of web applications for bioinformatics cannot be overstated. They are essential tools that allow bioinformaticians to access, view, and analyze data from anywhere in the world. They provide a user-friendly interface that makes it easy to view and analyze data. They allow bioinformaticians to share data and collaborate with other scientists. They can be used to create custom tools that are specific to the needs of a particular project. And they can be used to create public databases that are accessible to anyone with an internet connection.
+
+''')
