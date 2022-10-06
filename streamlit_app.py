6ed9142edc001ee687af7e4754cec7bb9a9a0aa8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-10-06 20:12:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,12 +160,16 @@ st.markdown('''
 1. Li H, Tamang T, Nantasenamat C. Toward insights on antimicrobial selectivity
 of host defense peptides via machine learning model interpretation. Genomics.
 2021;113(6):3851-3863.
+
 2. Schaduangrat N, Malik AA, Nantasenamat C. ERpred: a web server for the
 prediction of subtype-specific estrogen receptor antagonists. PeerJ. 2021;9:e11716.
+
 3. Schaduangrat N, Lampa S, Simeon S, Gleeson MP, Spjuth O, Nantasenamat C.
 Towards reproducible computational drug discovery. J Cheminform. 2020;12(1):9. 
+
 4. Li H, Nantasenamat C. Toward insights on determining factors for high
 activity in antimicrobial peptides via machine learning. PeerJ. 2019;7:e8265.
+
 5. Suvannang N, Preeyanon L, Malik AA, Schaduangrat N, Shoombuatong W,
 Worachartcheewan A, Tantimongcolwat T, Nantasenamat C. Probing the origin of
 estrogen receptor alpha inhibition via large-scale QSAR study. RSC Adv.
