4150a61244ce875ade484bcbfe07646491b69ef8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-10-06 17:34:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,10 +108,9 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 ''')
 
 selected_q = st.multiselect('Select Q', ['q1', 'q2', 'q3', 'q4'], ['q1', 'q2', 'q3', 'q4'])
-selected_company = st.multiselect('Select company', ['goog', 'fb', 'ms', 'amazon'], ['goog', 'fb', 'ms', 'amazon'])
 plost.bar_chart(
         data=datasets['stocks'],
-        bar=selected_company,
+        bar='company',
         value=selected_q,
         group='value',
         color='company',
