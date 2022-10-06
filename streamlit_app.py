2625b478069dc50005b6db6d36df356179eb6452
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-10-06 17:30:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import plost
 
+# Modified from Plost
 @st.cache
 def get_datasets():
     N = 50
@@ -106,13 +107,14 @@ st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh tortor id aliquet lectus proin nibh nisl condimentum id. Laoreet id donec ultrices tincidunt. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Cursus turpis massa tincidunt dui ut ornare lectus sit. Quis vel eros donec ac odio tempor. Quis auctor elit sed vulputate mi. Feugiat sed lectus vestibulum mattis. Sed vulputate odio ut enim blandit volutpat. Aliquam eleifend mi in nulla. Egestas sed tempus urna et. Nisl vel pretium lectus quam id leo. Quam elementum pulvinar etiam non quam lacus suspendisse. Lectus magna fringilla urna porttitor rhoncus. Vitae tempus quam pellentesque nec nam aliquam sem et. Mauris in aliquam sem fringilla ut morbi tincidunt augue interdum.
 ''')
 
-selected_q = st.multiselect('Select data', ['q1', 'q2', 'q3', 'q4'], ['q1', 'q2', 'q3', 'q4'])
+selected_q = st.multiselect('Select Q', ['q1', 'q2', 'q3', 'q4'], ['q1', 'q2', 'q3', 'q4'])
+selected_company = mt.multiselect('Select company', ['goog', 'fb', 'ms', 'amazon'], ['goog', 'fb', 'ms', 'amazon'])
 plost.bar_chart(
         data=datasets['stocks'],
         bar='company',
         value=selected_q,
         group='value',
-        color='company',
+        color=selected_company,
         legend=None,
     )
 st.caption('**Figure 2.** Bar chart of stocks data.')
